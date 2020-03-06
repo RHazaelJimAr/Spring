7d0a0eeda49ffb89bc6516c1f7a8d68a574641fa
Hazael <a01251333@itesm.mx>
2020-03-06 16:21:37 -0600

    hola prof

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -16,6 +16,6 @@ public class DemoApplication {
 
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Bonjour %s!", name);
+		return String.format("Gutentag %s!", name);
 	}
 }
